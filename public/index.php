50842f2e6e3c59a0bf3d00b3fc278109e1199f6e
= <info@joshlockhart.com>
2012-09-30 12:56:05 -0400

    Update for Slim 2

--- public/index.php
+++ public/index.php
@@ -2,26 +2,25 @@
 require '../vendor/autoload.php';
 
 // Prepare app
-$app = new Slim(array(
+$app = new \Slim\Slim(array(
     'templates.path' => '../templates',
     'log.level' => 4,
     'log.enabled' => true,
-    'log.writer' => new Log_FileWriter(array(
+    'log.writer' => new \Slim\Extras\Log\DateTimeFileWriter(array(
         'path' => '../logs',
         'name_format' => 'y-m-d'
     ))
 ));
 
 // Prepare view
-$twigView = new View_Twig();
-$twigView->twigOptions = array(
+\Slim\Extras\Views\Twig::$twigOptions = array(
     'charset' => 'utf-8',
     'cache' => realpath('../templates/cache'),
     'auto_reload' => true,
     'strict_variables' => false,
     'autoescape' => true
 );
-$app->view($twigView);
+$app->view(new \Slim\Extras\Views\Twig());
 
 // Define routes
 $app->get('/', function () use ($app) {
