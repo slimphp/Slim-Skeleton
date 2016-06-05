64af4e8d414ef94a895f7b444f6e314d250d4b8d
tsia <github@tsia.de>
2016-06-05 22:49:42 +0200

    updated static file handling for built-in PHP server

--- public/index.php
+++ public/index.php
@@ -2,7 +2,8 @@
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
-    $file = __DIR__ . $_SERVER['REQUEST_URI'];
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
     if (is_file($file)) {
         return false;
     }
