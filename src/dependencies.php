af7279d4fa21ffb68eb76213f80fed1fd303f0dc
Armin Vieweg <armin@v.ieweg.de>
2016-06-29 22:23:36 +0200

    Respect level setting and fall back to DEBUG

--- src/dependencies.php
+++ src/dependencies.php
@@ -14,6 +14,6 @@ $container['logger'] = function ($c) {
     $settings = $c->get('settings')['logger'];
     $logger = new Monolog\Logger($settings['name']);
     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
+    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level'] ?: \Monolog\Logger::DEBUG));
     return $logger;
 };
