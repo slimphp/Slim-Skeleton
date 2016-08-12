3f22dea7f798ca253917e9c866da0c83ea56412e
Rob Allen <rob@akrabat.com>
2016-08-12 08:03:31 +0100

    Add comment at top of BaseTestCase

--- tests/functional/BaseTestCase.php
+++ tests/functional/BaseTestCase.php
@@ -7,6 +7,12 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Http\Environment;
 
+/**
+ * This is an example class that shows how you could set up a method that
+ * runs the application. Note that it doesn't cover all use-cases and is
+ * tuned to the specifics of this skeleton app, so if your needs are
+ * different, you'll need to change it.
+ */
 class BaseTestCase extends \PHPUnit_Framework_TestCase
 {
     /**
