5469d650d1418a7fa1e0abf8b7a7ad8b5e02cfba
Jehong Ahn <toonitw@gmail.com>
2016-12-27 14:42:10 +0900

    util

---
+++ classes/Util.php
@@ -0,0 +1,14 @@
+<?php
+include_once '../base/Util.php';
+
+class Util Extends Base\Util {
+
+  protected $ci;
+
+  public function __construct($ci) {
+    $this->ci = $ci;
+  }
+
+
+
+}
--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,5 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+$container['util'] = function ($c) { return new Util($c); };
