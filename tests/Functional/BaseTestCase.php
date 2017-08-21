85d7aef572b8ba27293ca94406bd65db6f360b2e
Luis Ferro <lferro9000@gmail.com>
2017-08-21 07:43:21 +0100

    Update Composer to add phpunit version ^6.0 and update BaseTestCase to use namespace.

--- tests/Functional/BaseTestCase.php
+++ tests/Functional/BaseTestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Functional;
 
+use PHPUnit\Framework\TestCase;
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -13,7 +14,7 @@ use Slim\Http\Environment;
  * tuned to the specifics of this skeleton app, so if your needs are
  * different, you'll need to change it.
  */
-class BaseTestCase extends \PHPUnit_Framework_TestCase
+class BaseTestCase extends TestCase
 {
     /**
      * Use middleware when running application?
