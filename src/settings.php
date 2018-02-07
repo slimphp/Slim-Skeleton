43387f4989c0e43e18db48c8ed9cca8f0452dcfa
Owais Akbani <owais.akbani92@gmail.com>
2018-02-07 14:14:34 +0500

    Fix settings.php
    Please note that the settings with given config do not work. 
    Slim expects a settings array. Not an array of 'settings' inside an array.

--- src/settings.php
+++ src/settings.php
@@ -1,6 +1,5 @@
 <?php
 return [
-    'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
@@ -15,5 +14,4 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
-    ],
 ];
