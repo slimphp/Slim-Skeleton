8f3b36f46b1ca4ed030deb6e0941fa9a1d31ba73
Muhaimin CS <muhaimincs@gmail.com>
2018-07-27 01:23:28 +0800

    Align with documentation

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
 // Routes
 
