8d8916453017ec9c3b7161b4d71536327484356e
Pierre Bérubé <pierre@lgse.com>
2019-04-23 01:43:55 -0600

    add index.php file and app directory files

---
+++ app/routes.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    $app->group('/users', function (Group $group) use ($container) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
+};
--- public/index.php
+++ public/index.php
@@ -1,33 +1,71 @@
 <?php
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
-    }
-}
+declare(strict_types=1);
+
+use App\Application\Handlers\HttpErrorHandler;
+use App\Application\Handlers\ShutdownHandler;
+use App\Application\ResponseEmitter\ResponseEmitter;
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Slim\Factory\ServerRequestCreatorFactory;
+use Slim\Middleware\RoutingMiddleware;
+use Slim\Middleware\ErrorMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-session_start();
+// Instantiate PHP-DI Container
+$container = new Container();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
-$app = new \Slim\App($settings);
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+// Set up settings
+$settings = require __DIR__ . '/../app/settings.php';
+$settings($app);
 
 // Set up dependencies
-$dependencies = require __DIR__ . '/../src/dependencies.php';
+$dependencies = require __DIR__ . '/../app/dependencies.php';
 $dependencies($app);
 
 // Register middleware
-$middleware = require __DIR__ . '/../src/middleware.php';
+$middleware = require __DIR__ . '/../app/middleware.php';
 $middleware($app);
 
+// Set up repositories
+$repositories = require __DIR__ . '/../app/repositories.php';
+$repositories($app);
+
 // Register routes
-$routes = require __DIR__ . '/../src/routes.php';
+$routes = require __DIR__ . '/../app/routes.php';
 $routes($app);
 
-// Run app
-$app->run();
+/** @var bool $displayErrorDetails */
+$displayErrorDetails = $container->get('settings')['displayErrorDetails'];
+
+// Create Request object from globals
+$serverRequestCreator = ServerRequestCreatorFactory::create();
+$request = $serverRequestCreator->createServerRequestFromGlobals();
+
+// Create Error Handler
+$responseFactory = $app->getResponseFactory();
+$errorHandler = new HttpErrorHandler($responseFactory);
+
+// Create Shutdown Handler
+$shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
+register_shutdown_function($shutdownHandler);
+
+// Add Routing Middleware
+$routeResolver = $app->getRouteResolver();
+$routingMiddleware = new RoutingMiddleware($routeResolver);
+$app->add($routingMiddleware);
+
+// Add Error Middleware
+$callableResolver = $app->getCallableResolver();
+$errorMiddleware = new ErrorMiddleware($callableResolver, $responseFactory, $displayErrorDetails, false, false);
+$errorMiddleware->setDefaultErrorHandler($errorHandler);
+$app->add($errorMiddleware);
+
+// Run App & Emit Response
+$response = $app->handle($request);
+$responseEmitter = new ResponseEmitter();
+$responseEmitter->emit($response);
