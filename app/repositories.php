f1c36031707147b9fcf857ad129df961b9d19d6c
ssnepenthe <ssnepenthe@users.noreply.github.com>
2019-08-06 11:16:31 -0700

    Fix indentation - tabs to spaces

--- app/repositories.php
+++ app/repositories.php
@@ -7,7 +7,7 @@ use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
     // Here we map our UserRepository interface to its in memory implementation
-	$containerBuilder->addDefinitions([
-		UserRepository::class => \DI\autowire(InMemoryUserRepository::class),
-	]);
+    $containerBuilder->addDefinitions([
+        UserRepository::class => \DI\autowire(InMemoryUserRepository::class),
+    ]);
 };
