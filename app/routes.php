3ba4d97b5bee002385859357ab3107df99df37f3
Pierre Bérubé <pierre@lgse.com>
2019-08-18 11:12:13 -0600

    add default / route

--- app/routes.php
+++ app/routes.php
@@ -3,11 +3,18 @@ declare(strict_types=1);
 
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
     $container = $app->getContainer();
+
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
 
     $app->group('/users', function (Group $group) use ($container) {
         $group->get('', ListUsersAction::class);
