e28356c094d228b7bca69761672a863e90ea799b
Raffster <raffster.dev@gmail.com>
2019-10-04 14:52:13 -0300

    add missing call to ob_clean

--- src/Application/Handlers/ShutdownHandler.php
+++ src/Application/Handlers/ShutdownHandler.php
@@ -76,6 +76,7 @@ class ShutdownHandler
             $exception = new HttpInternalServerErrorException($this->request, $message);
             $response = $this->errorHandler->__invoke($this->request, $exception, $this->displayErrorDetails, false, false);
 
+            ob_clean();
             $responseEmitter = new ResponseEmitter();
             $responseEmitter->emit($response);
         }
