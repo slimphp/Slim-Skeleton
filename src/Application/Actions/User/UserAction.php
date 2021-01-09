85aa07af300ed70c85a00055d56e8e81ed2da130
Agung Nur Aprianto <agungaprianto96@gmail.com>
2021-01-09 11:17:02 +0700

    remove Unused Alias

--- src/Application/Actions/User/UserAction.php
+++ src/Application/Actions/User/UserAction.php
@@ -3,12 +3,12 @@ declare(strict_types=1);
 
 namespace App\Application\Actions\User;
 
-use App\Application\Actions\Action as parentAlias;
+use App\Application\Actions\Action;
 use App\Domain\Settings\SettingsInterface;
 use App\Domain\User\UserRepository;
 use Psr\Log\LoggerInterface;
 
-abstract class UserAction extends parentAlias
+abstract class UserAction extends Action
 {
     /**
      * @var UserRepository
