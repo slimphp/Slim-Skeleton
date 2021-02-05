88ba844e2495e04e5636b2dc1bc5695940ce68e6
Antonino <45453064+AntoninoM90@users.noreply.github.com>
2021-02-05 08:52:23 +0100

    Add "logError" and "logErrorDetails" on app/settings.php

--- app/settings.php
+++ app/settings.php
@@ -13,6 +13,8 @@ return function (ContainerBuilder $containerBuilder) {
         SettingsInterface::class => function () {
             return new Settings([
                 'displayErrorDetails' => true, // Should be set to false in production
+                'logError'            => false,
+                'logErrorDetails'     => false,
                 'logger' => [
                     'name' => 'slim-app',
                     'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
