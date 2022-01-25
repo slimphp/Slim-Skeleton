5b104b3c7f7bfa64c0e7823328b54848dc8f4a2d
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-01-25 16:53:44 +0300

    Use null coalescing operator

--- src/Application/ResponseEmitter/ResponseEmitter.php
+++ src/Application/ResponseEmitter/ResponseEmitter.php
@@ -15,7 +15,7 @@ class ResponseEmitter extends SlimResponseEmitter
     public function emit(ResponseInterface $response): void
     {
         // This variable should be set to the allowed host from which your API can be accessed with
-        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+        $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
         $response = $response
             ->withHeader('Access-Control-Allow-Credentials', 'true')
