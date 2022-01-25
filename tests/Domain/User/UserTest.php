aaf1a3c5c5e724872d85851c50e3d3654e6ed84c
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-01-25 16:54:00 +0300

    Declare return type

--- tests/Domain/User/UserTest.php
+++ tests/Domain/User/UserTest.php
@@ -9,7 +9,7 @@ use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    public function userProvider()
+    public function userProvider(): array
     {
         return [
             [1, 'bill.gates', 'Bill', 'Gates'],
