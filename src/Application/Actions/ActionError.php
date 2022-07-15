1444844611fb9173460c194c5562cc459dbabb25
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2022-07-15 08:56:13 +0100

    Remove optional type description ActionError

--- src/Application/Actions/ActionError.php
+++ src/Application/Actions/ActionError.php
@@ -22,7 +22,7 @@ class ActionError implements JsonSerializable
 
     private string $description;
 
-    public function __construct(string $type, ?string $description)
+    public function __construct(string $type, string $description)
     {
         $this->type = $type;
         $this->description = $description;
@@ -44,7 +44,7 @@ class ActionError implements JsonSerializable
         return $this->description;
     }
 
-    public function setDescription(?string $description = null): self
+    public function setDescription(string $description): self
     {
         $this->description = $description;
         return $this;
