f8b52d714bc7ae0b7caaa0da7e7ab520ac8766c1
Alejandro Pérez Batanero <alexgt9@gmail.com>
2022-10-20 14:29:43 +0200

    Updates routes for correct not found routes

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,7 @@ use App\Application\Actions\User\ViewUserAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
+use Slim\Exception\HttpNotFoundException;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
@@ -23,5 +24,9 @@ return function (App $app) {
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
+    });
+
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+        throw new HttpNotFoundException($request);
     });
 };
